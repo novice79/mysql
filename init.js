87e36255e1bd4e0fe425b0af0d86403c89e5d32f
novice <david@cninone.com>
2019-01-05 20:35:31 +0800

    fix sql error about flush privileges;

--- init.js
+++ init.js
@@ -64,30 +64,21 @@ const MYSQL_PASSWORD = process.env.MYSQL_PASSWORD || 'freego';
 const paras = process.argv.slice(2)
 if (paras.length == 0) {
     init_sql =
-        `
+    `
     CREATE USER IF NOT EXISTS 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}' ;
     GRANT ALL ON *.* TO 'root'@'%' WITH GRANT OPTION;
     CREATE USER IF NOT EXISTS 'david'@'%' IDENTIFIED BY 'freego';
     GRANT ALL ON *.* TO 'david'@'%';
     CREATE USER IF NOT EXISTS 'slaveuser'@'%' IDENTIFIED WITH sha256_password BY 'freego';
     GRANT REPLICATION SLAVE ON *.* TO 'slaveuser'@'%';
+    ${MYSQL_DATABASE ? `CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE} ;` : ''}
+    ${MYSQL_USER ? `CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';` : ''}
+    ${MYSQL_DATABASE && MYSQL_USER ? `GRANT ALL ON *.* TO '${MYSQL_USER}'@'%';` : ''}
+    FLUSH PRIVILEGES;
     `;
-    if (MYSQL_DATABASE) {
-        init_sql += `CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE} ;`;
-    }
-    if (MYSQL_USER) {
-        init_sql += `
-        CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
-        `;
-    }
-    if (MYSQL_DATABASE && MYSQL_USER) {
-        init_sql += `GRANT ALL ON *.* TO '${MYSQL_USER}'@'%';`;
-    }
-    init_sql += 'FLUSH PRIVILEGES;';
-
 } else {
     init_sql =
-        `
+    `
     CHANGE MASTER TO
     MASTER_HOST='${paras[0]}',
     MASTER_PORT=3306,
