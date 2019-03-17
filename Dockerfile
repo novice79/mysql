9715ad5ee387447a4f9a10f29f53c717b2d676b6
novice <david@cninone.com>
2019-03-17 13:10:42 +0800

    change init script to bash

--- Dockerfile
+++ Dockerfile
@@ -9,14 +9,15 @@ ENV LANG       en_US.UTF-8
 ENV LC_ALL	   "C.UTF-8"
 ENV LANGUAGE   en_US:en
 
-RUN apt-get update -y && apt-get install -y tzdata \
-	&& rm -rf /var/lib/apt/lists/*
+RUN apt-get update -y && apt-get install -y tzdata locales procps net-tools \
+	&& rm -rf /var/lib/apt/lists/* \
+	&& locale-gen en_US.UTF-8 zh_CN.UTF-8
 
 ENV TZ=Asia/Chongqing
 RUN ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 
-COPY init /
+COPY init.sh /
 RUN rm -f /var/lib/mysql/auto.cnf
 EXPOSE 3306 33060
 
-ENTRYPOINT ["/init"]
+ENTRYPOINT ["/init.sh"]
---
+++ init.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+log () {
+    printf "[%(%Y-%m-%d %T)T] %s\n" -1 "$*"
+}
+
+
+chown -R mysql:mysql /var/lib/mysql
+if [ ! -d /var/lib/mysql/mysql ]; then
+    rm -rf /var/lib/mysql/*
+    mysqld --initialize --user=mysql --datadir=/var/lib/mysql
+fi
+sql_init_file='/tmp/mysql-init.sql'
+s_id=1
+if [ "$#" -ne 1 ]; then
+    log "start as master db, and take into account env parameters"
+    # get environment variables:
+    log "MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD:=freego}"
+    log "MYSQL_USER=${MYSQL_USER:=david}"
+    log "MYSQL_PASSWORD=${MYSQL_PASSWORD:=freego}"
+    log "MYSQL_DATABASE=${MYSQL_DATABASE:=lemp}"
+    cat <<EOT > $sql_init_file
+CREATE USER IF NOT EXISTS 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}' ;
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+GRANT ALL ON *.* TO 'root'@'%' WITH GRANT OPTION;
+CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED WITH mysql_native_password BY '${MYSQL_PASSWORD}';
+GRANT ALL ON *.* TO '$MYSQL_USER'@'%';
+CREATE USER IF NOT EXISTS 'slaveuser'@'%' IDENTIFIED WITH sha256_password BY 'freego';
+GRANT REPLICATION SLAVE ON *.* TO 'slaveuser'@'%';
+CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
+FLUSH PRIVILEGES;
+EOT
+else
+    log "start as slave db, ignore all env parameters"
+    cat <<EOT > $sql_init_file
+CHANGE MASTER TO
+MASTER_HOST='$1',
+MASTER_PORT=3306,
+MASTER_USER='slaveuser',
+MASTER_PASSWORD='freego',
+MASTER_AUTO_POSITION=1;
+START SLAVE;
+EOT
+    EX_PARA="--master-info-repository=TABLE"
+    s_id=$((2 + RANDOM))
+fi
+
+mysqld --init-file="${sql_init_file}" --user=root --server-id=${s_id} --log-bin=mysql-bin --gtid-mode=ON --enforce-gtid-consistency=true --log-slave-updates ${EX_PARA}
